19664df6f9b970e1fb8de907dfc8cfa9239cf788
kfadnavis <kfadnavis@vmware.com>
2022-07-20 21:18:00 +0530

    Navigation

---
+++ src/app/assignment/admin-assignment/admin-assignment.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AdminAssignmentService } from './admin-assignment.service';
+
+describe('AdminAssignmentService', () => {
+  let service: AdminAssignmentService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AdminAssignmentService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
