c49f6dc551d071980190c4288f49c97c8b8d59b5
Shashi Sad <saadshashi25@gmail.com>
2022-07-26 12:36:51 +0530

    routing

---
+++ src/app/shared-components/admin-side-nav/admin-side-nav.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminSideNavComponent } from './admin-side-nav.component';
+
+describe('AdminSideNavComponent', () => {
+  let component: AdminSideNavComponent;
+  let fixture: ComponentFixture<AdminSideNavComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdminSideNavComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdminSideNavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared-components/admin-side-nav/admin-side-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-side-nav',
+  templateUrl: './admin-side-nav.component.html',
+  styleUrls: ['./admin-side-nav.component.css']
+})
+export class AdminSideNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
